bec0346bdc1cfd11ac1c09c1a29fec6bb66eaf97
Thomas Ryste <thomas.ryste@gmail.com>
2025-02-26 19:08:18 +0000

    cleanup code

--- testbot.py
+++ testbot.py
@@ -9,10 +9,10 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-# Sett opp logging
+# Logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# Sett API-nøkler
+# API and other keys
 load_dotenv()
 
 TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
@@ -42,72 +42,3 @@ if holders_div:
 else:
     print("Holders information not found.")
 
-
-# Filnavn for lagring av samtaler
-#IMAGE_FILE = "fleabone_image.json"
-#user_states = {}
-
-# Håndter bildefil-lagring
-#def load_image():
-#    if os.path.exists(IMAGE_FILE):
-#        with open(IMAGE_FILE, "r") as file:
-#            return json.load(file).get("image_id", None)
-#    return None
-
-#def save_image(image_id):
-#    with open(IMAGE_FILE, "w") as file:
-#        json.dump({"image_id": image_id}, file)
-
-# Last inn eksisterende samtale
-#image_id = load_image()
-
-# Kommando for å oppdatere bildet
-#async def update_image(update: Update, context):
-#    user_id = update.message.from_user.id
-#    user_states[user_id] = "awaiting_image"
-#    await update.message.reply_text("Vennligst send meg et nytt bilde eller en GIF som skal brukes fremover.")
-
-# Håndter mottak av bilde eller GIF
-#async def handle_media(update: Update, context):
-#    user_id = update.message.from_user.id
-
-    #if user_states.get(user_id) == "awaiting_image":
-        #if update.message.photo:
-            #new_image_id = update.message.photo[-1].file_id
-        #elif update.message.animation:
-            #new_image_id = update.message.animation.file_id
-        #else:
-            #await update.message.reply_text("Dette er ikke et gyldig bilde eller GIF. Prøv igjen.")
-            #return
-
-        #save_image(new_image_id)
-        #global image_id
-        #image_id = new_image_id
-        #await update.message.reply_text("Bilde/GIF er oppdatert og vil bli brukt i fremtidige meldinger.")
-        #user_states[user_id] = None
-
-# Håndter meldinger og legg til bildet
-#async def respond(update: Update, context):
-#    global conversation_history, image_id
-#
-#    user_message = update.message.text
-#    bot_username = f"@{context.bot.username}"
-#    
-#    if "bone" not in user_message and bot_username not in user_message:
-#        return
-#
-#    if image_id:
-#        await context.bot.send_photo(chat_id=update.message.chat_id, photo=image_id, caption=bot_reply)
-#    else:
-#        await update.message.reply_text(bot_reply)
-
-# Hovedfunksjon
-#if __name__ == '__main__':
-#    application = ApplicationBuilder().token(TELEGRAM_API_TOKEN).build()
-
-#    application.add_handler(CommandHandler('updateimage', update_image))
-#    application.add_handler(MessageHandler(filters.PHOTO | filters.ANIMATION, handle_media))
-#    application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), respond))
-
-#    application.run_polling()
-#    application.idle()
